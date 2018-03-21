50c67019076cb3b6331cd77db321ace3f58201b7
Wangxin Liu <flybird1987@gmail.com>
2018-03-21 22:13:07 +0100

    can train, can predict

--- mnist_de_encoder_daveyd.py
+++ mnist_de_encoder_daveyd.py
@@ -11,17 +11,13 @@ import tensorflow.contrib.layers as layers
 from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.contrib import framework
 from tensorflow.contrib.framework.python.ops.arg_scope import add_arg_scope
-import prepare_data
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
 def lenet_with_scope(features,labels,mode):
     input_layer = tf.reshape(features["x"],[-1,28,28,1])
-
-    if mode is not tf.estimator.ModeKeys.PREDICT:
-        groundtruth = tf.reshape(labels,[-1,28,28,1])
-        shp = groundtruth.get_shape()
-
+    groundtruth = tf.reshape(labels,[-1,28,28,1])
+    sp = input_layer.get_shape()
     arg_scope_conv2d = tf.contrib.framework.arg_scope([layers.conv2d],
                                         kernel_size=[5,5],
                                         activation_fn = tf.nn.leaky_relu,
@@ -43,10 +39,10 @@ def lenet_with_scope(features,labels,mode):
                 pool2 = tf.contrib.layers.max_pool2d(conv2,kernel_size=[2,2],stride=2)
                 pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
 
-                dense1 = tf.contrib.layers.fully_connected(inputs=pool2_flat, num_outputs=7*7*128)
-                dense2 = tf.contrib.layers.fully_connected(inputs=dense1, num_outputs=7*7*128)
+                dense1 = tf.contrib.layers.fully_connected(inputs=pool2_flat, num_outputs=2)
+                dense2 = tf.contrib.layers.fully_connected(inputs=dense1, num_outputs=7*7*64)
 
-                dense_unflat = tf.reshape(dense2,[-1,7,7,128])
+                dense_unflat = tf.reshape(dense2,[-1,7,7,64])
 
                 conv21 = tf.contrib.layers.conv2d(dense_unflat,num_outputs=64)
                 deconv21 = tf.contrib.layers.conv2d_transpose(conv21,num_outputs=32)
@@ -56,20 +52,18 @@ def lenet_with_scope(features,labels,mode):
 									     normalizer_fn=None)                
                 
                 reconstruct_probs = tf.nn.sigmoid(reconstruct_logits,name = "softmax")
-               
+
+                tf.summary.image("Diff",tf.abs(reconstruct_probs-groundtruth))      
+                tf.summary.image("Result",reconstruct_probs)
+                tf.summary.image("Origin",groundtruth)
+
                 reconstruction = {
                         "probabilities":reconstruct_probs
                         }
 
-                tf.summary.image("Result",reconstruct_probs)
-
                 if mode == tf.estimator.ModeKeys.PREDICT:
-                    return tf.estimator.EstimatorSpec(mode=mode, predictions=reconstruct_probs)
+                    return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
                 
-                tf.summary.image("Diff",tf.abs(reconstruct_probs-groundtruth))      
-                
-                tf.summary.image("Origin",groundtruth)
-
                 loss = tf.losses.absolute_difference(labels=groundtruth,predictions=reconstruct_probs)
 
                 if mode == tf.estimator.ModeKeys.TRAIN:
@@ -83,44 +77,51 @@ def lenet_with_scope(features,labels,mode):
                 return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=training_op)
 
 def main(args):
-    data_set = prepare_data.prepare_data_set("../train/","../train_cleaned/")    
-    test_set = prepare_data.prepare_data_set("../test/","../test/")
-    test_image_dirty = np.float32(np.asarray(test_set["dirty"]))/255.0
+    nums = []
+    for i in range(10):
+        img = np.zeros([28,28])        
+        cv2.putText(img,str(i),(5,25),cv2.FONT_HERSHEY_SIMPLEX,1,1,2,cv2.LINE_AA)
+        nums.append(np.float32(img.flatten()))
 
-    train_image_dirty = np.float32(np.asarray(data_set["dirty"]))/255.0
-    train_image_clean = np.float32(np.asarray(data_set["clean"]))/255.0
+    mnist = input_data.read_data_sets('MNIST_data')
+    train_image = mnist.train.images    
+    train_labels = np.array(mnist.train.labels,dtype=np.int32)
+
+    ##extract 10 pattern image
+    #pattern_image = []
+    #for i in range(0,10):
+    #    j = 0
+    #    while(train_labels[j]!=i):
+    #        j = j + 1
+    #    pattern_image.append(train_image[j])    
+    
+    train_image_labels = []
+    #construct ground truth
+    for i in range(0,train_labels.shape[0]):
+        train_image_labels.append(nums[train_labels[i]])
+
+    train_image_labels = np.asarray(train_image_labels)    
     #shp = np.shape(train_image_labels)
-    shape = train_image_clean.shape
-    type = train_image_clean.dtype
+
     estimator = tf.estimator.Estimator(
         model_fn = lenet_with_scope,        
-        model_dir = "../ckp")
+        model_dir = "D:/Study/DeepLearning/MNIST_Playground/mnist-playground/ckp")
 
     tensors_to_log = {} #{"probabilities": "softmax"}
     logging_hook = tf.train.LoggingTensorHook(
       tensors=tensors_to_log, every_n_iter=50)
     
     train_func = tf.estimator.inputs.numpy_input_fn(
-        x = {"x":train_image_dirty},
-        y = train_image_clean,
+        x = {"x":train_image},
+        y = train_image_labels,
         batch_size = 50,
         num_epochs=None,
-        shuffle=True)  
+        shuffle=True)
 
-    #estimator.train(
-    #    input_fn = train_func,
-    #    steps = 100,
-    #    hooks = [logging_hook])
-
-    predict_fun = tf.estimator.inputs.numpy_input_fn(
-        x = {"x":test_image_dirty},
-        y = test_image_dirty,shuffle=False)
-    
-    predictions = estimator.predict(input_fn = predict_fun)
-    results = list(predictions)
-    a = 0
-    
-
+    estimator.train(
+        input_fn = train_func,
+        steps = 200000,
+        hooks = [logging_hook])
 
 if __name__ == "__main__":
   tf.app.run()
--- prepare_data.py
+++ prepare_data.py
@@ -2,23 +2,25 @@ import cv2
 import os
 
 train_image_folder = "../train/"
-train_clean_folder = "../train_cleaned/"
+clean_images_folder = "../train_cleaned/"
 
-def prepare_training_set(size=28):
-    train_image_files = os.listdir(train_image_folder)
-    train_images = []
-    train_clean = []
-    for i in range(0,len(train_image_files)):
-        curr_image = cv2.imread(train_image_folder + train_image_files[i])
-        curr_clean = cv2.imread(train_clean_folder + train_image_files[i])
+def prepare_data_set(folder_dirty,folder_clean, size = 28):
+    image_files = os.listdir(folder_dirty)
+    dirty_images = []
+    clean_images = []
+    for i in range(0,len(image_files)):
+        curr_image = cv2.imread(folder_dirty + image_files[i])
+        curr_clean = cv2.imread(folder_clean + image_files[i])
         curr_shape = curr_image.shape
         num_w = curr_shape[0]//size
         num_h = curr_shape[0]//size
         for x in range(num_w):
             for y in range(num_h):
-                train_images.append(curr_image[x*size:x*size+28,y*size:y*size+28,0])
-                train_clean.append(curr_clean[x*size:x*size+28,y*size:y*size+28,0])
-    return {"dirty":train_images,"clean":train_clean}
+                dirty_images.append(curr_image[x*size:x*size+28,y*size:y*size+28,0])
+                clean_images.append(curr_clean[x*size:x*size+28,y*size:y*size+28,0])
+    return {"dirty":dirty_images,"clean":clean_images}
+
+    
 
 
 
